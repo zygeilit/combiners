ae9ff84d22e331985cb56eeb39e479491c591012
abnercrack <1129187719@qq.com>
2018-09-02 22:47:14 +0800

    build:windows todo

--- packages/cert/cert-manager.js
+++ packages/cert/cert-manager.js
@@ -13,22 +13,28 @@ const options = {
     name: 'develop server',
 }
 // 先不优化 
+
+// todo 增加windows系统支持支持
+// certutil .exe  -addstore  Root  MyCert .cer  // TrustedPublisher
+// certutil .exe  -p  MyPassword  -importpfx  MyCert .pfx 
+// certutil .exe  -p  MyPassword  -user  -importpfx  MyCert .pfx 
+
 module.exports = {
-    addCert: function(path) {
+    addCert: function (path) {
         const command = `security add-trusted-cert -d -k /Library/Keychains/System.keychain ${path}`
         sudo.exec(command, options,
             (error, stdout, stderr) => {
                 if (error) throw error;
-                console.log('stdout: ' + stdout);
+                console.log('certificate installed successfully');
             }
         );
     },
-    removeCert: function() {
-        const command  = `security remove-trusted-cert -d ${path}`;
+    removeCert: function () {
+        const command = `security remove-trusted-cert -d ${path}`;
         sudo.exec(command, options,
             (error, stdout, stderr) => {
                 if (error) throw error;
-                console.log('stdout: ' + stdout);
+                console.log('certificate removed successfully');
             }
         );
     }
