b8a837d68b278de542cbf3b5e8aba01bde46e75e
abnercrack <1129187719@qq.com>
2018-09-04 14:05:54 +0800

    docs : 增加baner

--- packages/server/util/parse-domain.js
+++ packages/server/util/parse-domain.js
@@ -2,7 +2,7 @@ module.exports = function (domain) {
     // SNI 需要解析顶域
     const specialTld = ['.com.cn', '.net.cn', '.org.cn', '.gov.cn']
     let position = -2;
-    const hasSpecial = specialTld.filter(item => domain.lastIndexOf(item) > 0)
-    if (hasSpecial.length > 0) position = -3;
+    const hasSpecialTld = specialTld.filter(item => domain.lastIndexOf(item) > 0)
+    if (hasSpecialTld.length > 0) position = -3;
     return domain.split('.').slice(position).join('.')
 };
